3278083e1041af8e7b5da66a759dd360840bacbe
Marcel-TO <marcel@turobin-ort.at>
2023-04-25 21:49:36 +0200

    added another function call.

--- modules/main.py
+++ modules/main.py
@@ -5,9 +5,10 @@ from verse_interpreter import Interpreter
 
 
 text = "f(p:int,q:int):int :=  p + q; f(1,2)"
+text = "x:int; f(p:int,q:int):int := (p=32; p + q); f(x,2); x"
 # text = "z:int; z=7; y:=(31|5); x:=(7|22); (z,x,y)"
 # text = "x:int; if(x<20) then x else 333; x=7; x" #!!!! x=7 reverse
-text = "z:=x+y; x,y:int; x=7; y = 3;z"
+# text = "z:=x+y; x,y:int; x=7; y = 3;z"
 # text = "for{1..10}" # !!!!!
 # text = "for{3|4}"
 # text = "for{false?}"
