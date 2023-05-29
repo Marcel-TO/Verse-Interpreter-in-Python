032ca851188730720e24d95350bfb3722e092e23
Kariyampalli <christyk2001@gmail.com>
2023-05-29 21:39:56 +0200

    Verse executable for windows

---
+++ Executable/Verse_Win/verse_console/verse_interpreter/verse_interpreter.py
@@ -0,0 +1,34 @@
+from verse_parser.verse_parser import Parser
+from syntaxtree.symboltable.symboltable import SymbolTable
+from syntaxtree.nodes.nodes import *
+from structure.logger.logger import *
+
+class Interpreter:
+    def __init__(self, parser: Parser):
+        self.parser = parser
+        self.symboltable = SymbolTable(None)
+    
+    def reset_input(self):
+        self.parser.reset()
+    
+    def reset(self):
+        self.symboltable = SymbolTable(None)
+        self.parser.reset()
+
+
+    def interpret(self):
+        tree = self.parser.parse()
+        # --HIER GEÄNDERT damit er er auch beim error ein failnode zurück liefert
+        result = None
+        if tree != None:
+            contexts = Contexts([tree])
+            result = contexts.visit(self.symboltable)
+
+            self.symboltable.remove_all_except_self()
+            result =  contexts.visit(self.symboltable)
+            result = result.visit(self.symboltable)
+
+        if(result == None):
+            result = FailNode(Token(TokenTypes.FAIL, TokenTypes.FAIL.value))
+
+        return result
